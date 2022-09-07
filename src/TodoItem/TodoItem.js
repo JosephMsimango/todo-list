e7dd2ce495fb1b06904d8428cf980e43a28350d1
JosephMsimango <Josephmsimango98@gmail.com>
2022-09-07 08:28:04 +0200

    styling the todo item

--- src/TodoItem/TodoItem.js
+++ src/TodoItem/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './TodoItem.css';
 
 const TodoItem = ({todo}) => {
   const [check,setCheck] = useState(todo.complete);
@@ -8,9 +9,15 @@ const TodoItem = ({todo}) => {
   } 
   
   return (
-    <div>
-        <span>{todo.name}</span>
-        <input type="checkbox" value={check} onChange={(e)=>isComplete(e.target.value)}/>
+    <div className='container todo-item'>
+      <div className='row'>
+        <span className='todo-name col col-8'>
+          {todo.name}
+        </span>
+        <span className='col col-2'>
+          <input className='todo-checkbox' type="checkbox" value={check} onChange={(e)=>isComplete(e.target.value)}/>
+        </span>
+      </div>
     </div>
   )
 }
